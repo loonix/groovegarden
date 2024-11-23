37d43d1d8dd3dcdd061c99ab945919386a2b4442
Daniel Carneiro <daniel.carneiro@cads.co.uk>
2024-11-23 00:41:41 +0000

    feat: Update ApiService to handle token-based authentication and add song upload functionality

--- groovegarden_flutter/lib/services/api_service.dart
+++ groovegarden_flutter/lib/services/api_service.dart
@@ -3,14 +3,12 @@ import 'package:http/http.dart' as http;
 
 class ApiService {
   static String baseUrl = 'http://localhost:8081';
-  static String bearer = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE3MzIyMzA0ODUsInJvbGUiOiJhcnRpc3QiLCJ1c2VyX2lkIjoxfQ.4PsoGJlxQyxIKeuDuhYKO6VJtrxbcBbJgNDtXByGtBY';
+  static String bearer = ''; //'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE3MzIyMzA0ODUsInJvbGUiOiJhcnRpc3QiLCJ1c2VyX2lkIjoxfQ.4PsoGJlxQyxIKeuDuhYKO6VJtrxbcBbJgNDtXByGtBY';
   // Fetch songs from the backend
-  static Future<List<dynamic>> fetchSongs() async {
+  static Future<List<dynamic>> fetchSongs(String token) async {
     final response = await http.get(
       Uri.parse('$baseUrl/songs'),
-      headers: {
-        'Authorization': 'Bearer $bearer', // Replace with valid token
-      },
+      headers: {'Authorization': 'Bearer $token'},
     );
 
     if (response.statusCode == 200) {
@@ -31,6 +29,36 @@ class ApiService {
 
     if (response.statusCode != 200) {
       throw Exception('Failed to vote for song');
+    }
+  }
+
+  static Future<void> addSong(String title, String url, String token) async {
+    final response = await http.post(
+      Uri.parse('$baseUrl/add'),
+      headers: {
+        'Authorization': 'Bearer $token',
+        'Content-Type': 'application/json',
+      },
+      body: jsonEncode({'title': title, 'url': url}),
+    );
+
+    if (response.statusCode != 200) {
+      throw Exception('Failed to upload song: ${response.body}');
+    }
+  }
+
+  static Future<String> login(String email, String password) async {
+    final response = await http.post(
+      Uri.parse('$baseUrl/login'),
+      headers: {'Content-Type': 'application/json'},
+      body: jsonEncode({'email': email, 'password': password}),
+    );
+
+    if (response.statusCode == 200) {
+      final data = jsonDecode(response.body);
+      return data['token']; // Assuming the backend returns a token
+    } else {
+      throw Exception('Invalid email or password');
     }
   }
 }
