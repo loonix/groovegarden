1fe32af4251519a035e0cfb3b7ba325c986351ab
Daniel Carneiro <daniel.carneiro@cads.co.uk>
2024-11-23 00:40:10 +0000

    feat: Implement token handling in main app entry point for secure login

--- groovegarden_flutter/lib/main.dart
+++ groovegarden_flutter/lib/main.dart
@@ -1,25 +1,45 @@
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
+import 'package:groovegarden_flutter/utils/secure_storage.dart';
+import 'screens/login_screen.dart';
 import 'screens/home_screen.dart';
+import 'dart:html' as html;
 
-void main() async {
-  // Ensure Flutter is initialized
-  WidgetsFlutterBinding.ensureInitialized();
-
-  runApp(const GrooveGardenApp());
+void main() {
+  runApp(GrooveGardenApp());
 }
 
 class GrooveGardenApp extends StatelessWidget {
-  const GrooveGardenApp({super.key});
-
   @override
   Widget build(BuildContext context) {
+    // Check for token in URL
+    final url = html.window.location.href;
+    final uri = Uri.parse(url);
+    final token = uri.queryParameters['token'];
+    debugger();
+    // Store the token in secure storage if present
+    if (token != null) {
+      SecureStorage.saveToken(token);
+    }
+
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'GrooveGarden',
       theme: ThemeData(
         primarySwatch: Colors.green,
       ),
-      home: HomeScreen(),
+      // Navigate based on whether the token exists
+      home: FutureBuilder<String?>(
+        future: SecureStorage.getToken(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return Center(child: CircularProgressIndicator());
+          }
+          final savedToken = snapshot.data;
+          return savedToken != null ? HomeScreen(jwtToken: savedToken) : LoginScreen();
+        },
+      ),
     );
   }
 }
